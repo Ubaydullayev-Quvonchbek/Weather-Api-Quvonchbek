618066b11be1c43415eeb53b02b25d2fcedc094d
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-03-05 18:04:33 +0500

    Team Quvonchbek

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let input = document.querySelector('input')
-let button = document.querySelector('button')
+let input = document.querySelector('input');
+let button = document.querySelector('button');
 
 button.onclick = () => {
     let key = "84a3781201a7f9a9bda21e4f05b6c2f0"
@@ -11,25 +11,26 @@ button.onclick = () => {
             let gradus = document.querySelector('.gradus');
             let max = document.querySelector('h5');
             let min = document.querySelector('h3');
-            city.innerHTML = data.name
-            sunny_cloudy.innerHTML = data.weather[0].main
-            gradus.innerHTML = Math.ceil(data.main.temp) + "°"
-            max.innerHTML = Math.ceil(data.main.temp) + "°"
-            min.innerHTML = Math.floor(data.main.feels_like) + "°"
-            input.value = ""
+            city.innerHTML = data.name;
+            sunny_cloudy.innerHTML = data.weather[0].main;
+            gradus.innerHTML = Math.ceil(data.main.temp) + "°";
+            max.innerHTML = Math.ceil(data.main.temp) + "°";
+            min.innerHTML = Math.floor(data.main.feels_like) + "°";
+            input.value = "";
             console.log(data);
         }).catch((error) => {
-            alert("Bunday shahar mavjud emas")
-            let city = document.querySelector('h1');
+            alert("Bunday shahar mavjud emas");
+            let city = document.querySelector('h4');
             let sunny_cloudy = document.querySelector('h6');
             let gradus = document.querySelector('.gradus');
-            let max = document.getElementById('max');
-            let min = document.getElementById('min');
-            city.innerHTML = "London"
-            sunny_cloudy.innerHTML = "Cloudy"
-            gradus.innerHTML = "6°"
-            max = "6°"
-            min = "3°"
-            input.value = ""
+            let max = document.querySelector('h5');
+            let min = document.querySelector('h3');
+            city.innerHTML = "London";
+            sunny_cloudy.innerHTML = "CLOUDS";
+            gradus.innerHTML = "0°";
+            max.innerHTML = "0°";
+            min.innerHTML = "0°";
+            input.value = "";
+            console.log(data);
         })
 }
